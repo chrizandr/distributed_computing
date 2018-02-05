45a5d21fa776dd5db6de307c59311acbaa68039b
Chris <chris.g14@iiits.in>
2018-02-05 22:53:53 +0530

    Changes.

--- example.py
+++ example.py
@@ -1,81 +1,70 @@
 """Simulator."""
 import multiprocessing
 from random import randint
-from queue import Queue
+import sys
+
+processes = []
 
 
 class SimProcess(multiprocessing.Process):
     """Simulation of a Processor."""
 
-    def __init__(self, id_, message_queue=None, num_process=1, marker=""):
+    def __init__(self, id_, message_queue=[], num_process=1, marker=""):
         """Init."""
         multiprocessing.Process.__init__(self)
-        assert marker in ["L", "R"]
+        # assert marker in ["L0", "L1", "R0", "R1"]
         assert id_ > 0
         self.pid = id_
         self.messages = message_queue
-        if message_queue is None:
-            self.messages = Queue()
         self.num_process = num_process
         num = randint((0, 2*num_process))
-        self.storage = [num, num]
-        self.marker = marker
+        self.value = num
+        # self.marker = marker
+
+    def receive(self):
+        """Receive wait."""
+        counter = 0
+        while True:
+            if not self.messages:
+                for message in messages
+                message = self.messages
+                counter += 1
+
 
     def run(self):
         """Sorting logic."""
-        proc_name = self.name
-        while True:
-            next_task = self.task_queue.get()
-            if next_task is None:
-                # Poison pill means shutdown
-                print '%s: Exiting' % proc_name
-                self.task_queue.task_done()
-                break
-            print '%s: %s' % (proc_name, next_task)
-            answer = next_task()
-            self.task_queue.task_done()
-            self.result_queue.put(answer)
+        prev_ = self.id_ + 1 if self.id_ < self.num_process-1 else -1
+        next_ = self.id_ - 1 if self.id_ > 0 else -1
+        for i in range(self.num_process):
+            if self.id_ % 2 == i % 2:
+                network_send(Message(self.value, self.id_, prev_, i))
+                network_send(Message(self.value, self.id_, next_, i))
+                self.receive()
+
         return
 
 
 class Message():
     """Message class."""
 
-    def __init__(self, value, marker, from_id, to_id):
+    def __init__(self, value, from_id, to_id, round_):
         """Init."""
         self.value = value
-        self.marker = marker
+        # self.marker = marker
         self.from_id = from_id
         self.to_id = to_id
+        self.round = round_
 
 
 if __name__ == '__main__':
-    # Establish communication queues
+
+    num_process = int(sys.argv[1])
     tasks = multiprocessing.JoinableQueue()
     results = multiprocessing.Queue()
 
-    # Start consumers
-    num_consumers = multiprocessing.cpu_count() * 2
-    print 'Creating %d consumers' % num_consumers
-    consumers = [ Consumer(tasks, results)
-                  for i in range(num_consumers) ]
-    for w in consumers:
-        w.start()
-
-    # Enqueue jobs
-    num_jobs = 10
-    for i in xrange(num_jobs):
-        tasks.put(Task(i, i))
-
-    # Add a poison pill for each consumer
-    for i in xrange(num_consumers):
-        tasks.put(None)
-
-    # Wait for all of the tasks to finish
-    tasks.join()
-
-    # Start printing results
-    while num_jobs:
-        result = results.get()
-        print 'Result:', result
-        num_jobs -= 1
+    left_process = SimProcess(0, Queue(), num_process)
+    processes.append(left_process)
+    processes.extend([SimProcess(i+1, Queue(), num_process)
+                      for i in range(num_process - 2)])
+    right_process = SimProcess(num_process-1, Queue(), num_process)
+    processes.append(right_process)
